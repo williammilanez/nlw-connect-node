f6a40d30c0f083d9cd8f04aff8562836cdf187e8
William Milanez <william_milanez@hotmail.com>
2025-02-22 15:25:02 -0300

    aula03

---
+++ src/functions/get-subscriber-invites-count.ts
@@ -0,0 +1,13 @@
+import { redis } from '../redis/client'
+
+interface GetSubscriberInvitesCountParams {
+  subscriberId: string
+}
+
+export async function getSubscriberInvitesCount({
+  subscriberId,
+}: GetSubscriberInvitesCountParams) {
+  const count = await redis.zscore('referral:ranking', subscriberId)
+
+  return { count: count ? Number.parseInt(count) : 0 }
+}
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,11 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { env } from './env'
-import { accessInviteLinkRoute } from './routes/access-invite-link'
+import { accessInviteLinkRoute } from './routes/access-invite-link-route'
+import { getRankingRoute } from './routes/get-ranking-route'
+import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -35,6 +39,10 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
+app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
